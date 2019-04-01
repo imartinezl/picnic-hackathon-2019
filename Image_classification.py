45f712849bb960c9571c2582e868de0be3623a58
imartinezl <inigomlap@gmail.com>
2019-04-01 23:56:10 +0200

    Image paths and labels

--- Image_classification.py
+++ Image_classification.py
@@ -9,28 +9,49 @@ Created on Mon Apr  1 16:52:40 2019
 # Tensorflow tutorial on image classification and image load
 import tensorflow as tf
 tf.VERSION
- 
-import pathlib
 
-data_root_train = pathlib.Path('./train')
-data_root_test = pathlib.Path('./test')
 
-for item in data_root_train.iterdir():
-  print(item)
-
+import glob
+image_paths_train = glob.glob("./train/*")
+image_paths_test = glob.glob("./test/*")
 
 import random
-all_image_paths = list(data_root_train.glob('*'))
-all_image_paths = [str(path) for path in all_image_paths]
-random.shuffle(all_image_paths)
+random.shuffle(image_paths_train)
 
-image_count = len(all_image_paths)
+image_count = len(image_paths_train)
 image_count
 
 
 import IPython.display as display
-
 for n in range(3):
-  image_path = random.choice(all_image_paths)
+  image_path = random.choice(image_paths_train)
   display.display(display.Image(image_path))
   print()
+
+# Get labels
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train = pd.read_csv('train.tsv', sep='\t')
+test = pd.read_csv('test.tsv', sep='\t')
+
+label_to_index = dict((name, index) for index,name in enumerate(train.label.unique()))
+train['index'] = [label_to_index[label] for label in train.label]
+
+img_path = image_paths_train[0]
+img_path
+img_raw = tf.read_file(img_path)
+print(repr(img_raw)[:100]+"...")
+
+img_tensor = tf.image.decode_image(img_raw)
+print(img_tensor.shape)
+print(img_tensor.dtype)
+
+img_final = tf.image.resize_images(img_tensor, [192, 192])
+img_final = img_final/255.0
+print(img_final.shape)
+print(img_final.numpy().min())
+print(img_final.numpy().max())
